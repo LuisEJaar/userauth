9a22ab1eed591f6f3396441809bcc772e828d15d
LuisEJaar <luisjaarcoding@gmail.com>
2022-07-07 20:35:39 -0500

    Making Schema in User.js

---
+++ model/User.js
@@ -0,0 +1,23 @@
+// We set up our mongoose schema here (kinda like file structure, but for items in our database)
+
+const Mongoose = require("mongoose")
+const UserScheme = new Mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        requried: true,
+        minlength: 6,
+    }, 
+
+    role: {
+        type: String,
+        default: "Basic", 
+        required: true,
+    }
+
+})
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const connectDB = require("./db")
+const app = express()
+const PORT = 5000
+const ConnectDB = requrie('./db')
+
+connectDB()
+
+const server = app.listen(PORT, ()=> console.log(`Server connected to port ${PORT}`))
+
+process.on('unhandledRejection', err=> {
+    console.log(`An error occured: ${err.message}`)
+    server.close(() => process.exit(1))
+})
+
+
+
